ebd8bd64166b82e13fcbe40b514961e72ba7d410
jifraf5656 <jifraf@gmail.com>
2026-07-22 15:44:36 +0300

    Configure Vite target for iPadOS Safari compatibility and add Apple PWA meta tags

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: ['es2015', 'safari13', 'ios13'],
+    cssTarget: ['safari13', 'ios13']
+  }
 })
